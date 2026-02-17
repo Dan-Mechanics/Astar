46a0686a0fcb294eb51f9117da39b473669d9af3
Dan-Mechanics <github.tim@desmidt.org>
2026-02-17 21:14:53 +0100

    Update Astar.cs

--- Astar/Assets/Scripts/Astar.cs
+++ Astar/Assets/Scripts/Astar.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
+/// <summary>
+/// https://www.youtube.com/playlist?list=PLFt_AvWsXl0cq5Umv3pMC9SPnKjfp9eGW
+/// https://hku.instructure.com/courses/2268/files/152426?module_item_id=15787
+/// </summary>
 public class Astar
 {
     /// <summary>
     /// TODO: Implement this function so that it returns a list of Vector2Int positions which describes a path from the startPos to the endPos
     /// Note that you will probably need to add some helper functions
     /// </summary>
-    /// <param name="startPos"></param>
-    /// <param name="endPos"></param>
-    /// <param name="grid"></param>
-    /// <returns></returns>
     public List<Vector2Int> FindPathToTarget(Vector2Int startPos, Vector2Int endPos, Cell[,] grid)
     {
         return null;
